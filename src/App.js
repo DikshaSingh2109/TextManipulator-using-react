e29a513964183dde6027c3b0710bb5c51ebb5131
DikshaSingh2109 <dikshagtbit@gmail.com>
2024-02-15 15:38:45 +0530

    change in app.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
   return (
     <> 
   
-  <Router basename="">
+  <Router >
   <Navbar title= "TextUtils"  mode={mode} toggleMode={toggleMode}/>
   <Alert alert ={alert}/>
   <div className="container my-3">
